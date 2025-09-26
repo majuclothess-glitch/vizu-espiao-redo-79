b3ee49d3261ff52f825f549370caf7ff68c05bbd
majuclothess-glitch <maju.clothess@gmail.com>
2025-09-26 15:30:33 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./", // 👈 importante para GitHub Pages
+  base: "./", // 👈 IMPORTANTE para funcionar no GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
